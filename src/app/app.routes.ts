3bac251180bce3358dd6b099297320cfe8805d19
jafethBM <jafethbadillomartinez@gmail.com>
2025-10-30 18:46:45 -0600

    proyecto gifs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { SideMenuHeader } from './gifs/components/side-menu/side-menu-header/side-menu-header';
+import { SideMenuOptions } from './gifs/components/side-menu/side-menu-options/side-menu-options';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard/dashboard-page'),
+    children:[
+        {
+          path: 'search',
+          loadComponent: () =>
+            import('./gifs/pages/search/search-page'),
+        },
+        {
+          path: 'trending',
+          loadComponent: () =>
+            import('./gifs/pages/trending/trending-page'),
+        },
+        {
+          path: '**',
+          redirectTo: 'trending'
+        }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
